a514779444470f153a6bbe181e9429f3625a3a9e
Ashton Simmonds <ashton.simmonds@gmail.com>
2021-04-12 20:58:04 +0900

    Add files via upload
    Finding ways to apply things I learned recently.
    
    Making lists into iterator objects. 
    Using assert
    Using a function as a parameter
    Random range to force even numbers 
    I wanted to use yield but couldn't get that working.

---
+++ annotation-review.py
@@ -0,0 +1,27 @@
+import random
+def doIt(a:int) -> list:
+	myList= []
+	max = random.randrange(2,10,2)
+	# print (max)
+
+	for n in range (0,max):
+		myList.append(a)
+
+	return myList
+
+print(doIt.__annotations__)
+
+def doMore(x):
+	lengthOFList = (len(x))
+	# print(lengthOFList)
+	x.insert(lengthOFList//2,"foo")
+	music = iter(x)
+	print(x)
+	return music
+
+testIt = doMore(doIt(2))
+
+for n in testIt:
+	print (n)
+	assert n == 2
+
